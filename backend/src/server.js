91e325573530724d6005e95ed92a0cb202f8bd0d
unadkat7 <153261041+unadkat7@users.noreply.github.com>
2025-12-17 18:25:19 +0530

    auth-db-sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { ENV } from "../lib/env.js  ";
+import { ENV } from "../lib/env.js";
 import path from "path";
 import { connectDB } from "../lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express";
+import { inngest,functions } from "../lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest,functions}));
 
 app.get("/health",(req,res)=>{
     res.status(200).json({msg:"api is up and running"})
