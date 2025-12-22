da09528f5a6497f87080f727839428a121f3a74a
unadkat7 <153261041+unadkat7@users.noreply.github.com>
2025-12-22 16:02:20 +0530

    Landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,40 @@
+import HomePage from "./pages/HomePage";
 import {
-  SignedOut,
   SignedIn,
-  SignOutButton,
-  SignIn,
+  SignedOut,
   SignInButton,
+  SignOutButton,
   UserButton,
+  useUser,
 } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import ProblemPage from "./pages/ProblemPage";
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+
 function App() {
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
+
   return (
     <>
-      <h1 className="text-amber-500">Welcome to the app</h1>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button>Login</button>
-        </SignInButton>
-      </SignedOut>
+      <Routes>
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/problems"
+          element={isSignedIn ? <ProblemPage /> : <Navigate to="/" />}
+        />
+      </Routes>
 
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-
-      <UserButton></UserButton>
+      <Toaster position="top-right" reverseOrder={false} />
     </>
   );
 }
